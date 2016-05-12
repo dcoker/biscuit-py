533cff2c800842b10778a9927a2a31e46ab56fe2
Doug Coker <dcoker@gmail.com>
2016-05-12 13:50:44 -0700

    pep8 fixes

--- tests/biscuit_test.py
+++ tests/biscuit_test.py
@@ -4,11 +4,13 @@
 import binascii
 import collections
 import unittest
+
 import biscuit
 import os.path
 import yaml
 
-test_vector = collections.namedtuple('test_vector', ['key', 'nonce', 'plaintext', 'result'])
+test_vector = collections.namedtuple('test_vector',
+                                     ['key', 'nonce', 'plaintext', 'result'])
 
 
 class TestAesGcm256(unittest.TestCase):
@@ -47,7 +49,8 @@ class TestAesGcm256(unittest.TestCase):
 class TestBiscuit(unittest.TestCase):
     def test_algorithms(self):
         secrets = biscuit.biscuit()
-        with open(os.path.join(os.path.dirname(__file__), "hello.yaml"), "r") as fp:
+        with open(os.path.join(os.path.dirname(__file__),
+                               "hello.yaml"), "r") as fp:
             parsed = yaml.load(fp)
             secrets.update(parsed)
         self.assertEqual("hello", secrets.get("aes"))
@@ -55,10 +58,12 @@ class TestBiscuit(unittest.TestCase):
         self.assertEqual("hello", secrets.get("none"))
 
     def test_region_from_arn(self):
-        tests = {"arn:aws:kms:us-west-1:123456789012:key/37793df5-ad32-4d06-b19f-bfb95cee4a35": "us-west-1",
-                 "key/37793df5-ad32-4d06-b19f-bfb95cee4a35": None,
-                 "arn:aws:kms:us-west-1:123456789012:alias/biscuit-x": "us-west-1",
-                 "alias/biscuit-x": None}
+        prefix = "arn:aws:kms:us-west-1:123456789012:"
+        tests = {
+            prefix + "key/37793df5-ad32-4d06-b19f-bfb95cee4a35": "us-west-1",
+            "key/37793df5-ad32-4d06-b19f-bfb95cee4a35": None,
+            prefix + "alias/biscuit-x": "us-west-1",
+            "alias/biscuit-x": None}
         for arn, region in tests.iteritems():
             self.assertEqual(region, biscuit.region_from_arn(arn))
 
